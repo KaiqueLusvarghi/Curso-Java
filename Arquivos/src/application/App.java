2abee9d224d324757d9f743bbb70b42636d6b976
Kaique Ortolani Lusvarghi <99893722+KaiqueLusvarghi@users.noreply.github.com>
2022-12-16 08:01:20 -0300

    Add files via upload

---
+++ Arquivos/src/application/App.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+        File file = new File("C:\\Users\\alunos\\Desktop\\in.txt");
+        Scanner sc = null;
+
+        try{
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+
+            }
+        }
+        catch(IOException e){
+            System.out.println("Error " + e.getMessage());
+        }
+        finally{
+            if(sc != null){
+            sc.close();
+            }
+        }
+    }
+}
