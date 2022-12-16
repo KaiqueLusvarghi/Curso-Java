2dfff578e20b729064b95daa8bb92173c93e82fd
Kaique Ortolani Lusvarghi <99893722+KaiqueLusvarghi@users.noreply.github.com>
2022-12-16 09:14:08 -0300

    Bloco try-with-resources

---
+++ Arquivos III/src/application/App.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String path = "C:\\Users\\alunos\\Desktop\\in.txt ";
+      
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line = br.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error : " + e.getMessage());
+        } 
+    }
+}
